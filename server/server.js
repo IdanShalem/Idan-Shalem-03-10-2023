d5c95e9e4155635f98b302e2bb3c0dd0cb712c48
Idan Shalem <idansha@radware.com>
2023-10-15 20:14:58 +0300

    server changes

--- server/server.js
+++ server/server.js
@@ -40,10 +40,13 @@ sockets.use(io);
 
 app.use('/', routes);
 
+
+const __dirname1 = path.resolve();
+
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/build")));
+  app.use(express.static(path.join(__dirname1, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
   })
 } else {
   app.get("/", (req, res) => {
